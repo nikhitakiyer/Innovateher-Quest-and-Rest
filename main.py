509a6af5c69d520d1cf3dd6c48a7daa95a0a97dc
nikhitakiyer <nikhita.k.iyer@gmail.com>
2026-02-07 21:13:01 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print
+#
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+#Variables
+
+#Lists
+activities = ["take a 15 minute walk outside, do 30 jumping jacks, stretch for 10 minutes,  "]
+
+print("Quest & Rest Game Starting...")
+
+#opening window
+root = tk.Tk()
+root.title("Quest & Rest")
+root.geometry("600x400")
+root.mainloop()
